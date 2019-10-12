c59e66322bf73001a0f139b8c082d903a3214c8f
sanjay kumar <sanjaysan648@gmail.com>
2019-10-12 18:30:24 +0530

    Add files via upload

---
+++ gaming door.py
@@ -0,0 +1,12 @@
+from random import radiant
+for attempt in range(3):
+		print(“\n choose a door(1,2,3,4,5 or 6)
+choosendoor=input()
+choosendoor=int(choosendoor)
+winningdoor=randint(1,2)
+print(“the choosen door is “,choosendoor)
+print(“the winning door is”,winningdoor)
+if(choosendoor=winningdoor):
+print(“welldone”)
+Else:
+print(“unlucky!”)
